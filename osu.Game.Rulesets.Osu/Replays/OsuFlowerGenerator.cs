f8f11337ac35b3bd764bef0f094b7abd11ca6fdf
Galcatqwq <lihongkun399@gmail.com>
2025-08-06 01:06:56 +0800

    暂时禁用FlowerGenerator(Danser)

--- osu.Game.Rulesets.Osu/Replays/OsuFlowerGenerator.cs
+++ osu.Game.Rulesets.Osu/Replays/OsuFlowerGenerator.cs
@@ -1,4 +1,5 @@
-﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
+﻿/*
+// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
 using System;
@@ -192,3 +193,4 @@ namespace osu.Game.Rulesets.Osu.Replays
         #endregion
     }
 }
+*/
